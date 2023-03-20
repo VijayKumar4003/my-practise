595d8a12a104856051db2a5acd22b1be2f615a8b
VijayKumar4003 <38779300+VijayKumar4003@users.noreply.github.com>
2023-03-20 21:12:02 +0530

    init change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './Header.css';
+import Header from './components/Header';
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         Hello
+
+
+      <Header/>
+
+
       </header>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+
+      
+      </header>
+    </div>
+  );
+}
+
+export default Header;
